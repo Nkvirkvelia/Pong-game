d560e574dfce8369249b0fa04c495e4dab62f8cc
kiu_kvirkvelia.nana <kvirkvelia.nana@kiu.edu.ge>
2025-06-03 14:37:16 +0400

    Clean React template and set up project folders

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", paddingTop: "2rem" }}>
+      <h1>Multiplayer Pong Game</h1>
+      <p>Socket connection will appear here later.</p>
+    </div>
+  );
+}
+
+export default App;
